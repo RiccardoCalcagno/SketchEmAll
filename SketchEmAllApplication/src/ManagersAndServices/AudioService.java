a7323ada1b40b61dda61d7a638736e2248ca0372
Julien Marzal <julien.marzal@telecom-paris.fr>
2022-10-11 22:26:36 +0200

    All sounds implemented + documentation

--- SketchEmAllApplication/src/ManagersAndServices/AudioService.java
+++ SketchEmAllApplication/src/ManagersAndServices/AudioService.java
@@ -2,14 +2,21 @@ package ManagersAndServices;
 
 import javax.sound.sampled.*;
 import java.io.IOException;
+import java.util.Random;
 
 /**
- * Static class used handle sounds in the game
+ * Static class used handle sounds in the game. Can only open .wav sounds
  */
 public class AudioService {
 
+    /**
+     * Audio player
+     */
     private static Clip audioClip;
 
+    /**
+     * Initializes the audio player by getting authorization from the system.
+     */
     public static void initialize(){
         try {
             audioClip = AudioSystem.getClip();
@@ -18,8 +25,14 @@ public class AudioService {
         }
     }
 
+    /**
+     * Gets the sound from the resources folder
+     * @param soundName
+     *  Name of the file containing the sound.
+     * @return
+     *  Stream containing the sound
+     */
     private static AudioInputStream getSound(String soundName){
-
         try {
             return AudioSystem.getAudioInputStream(RepositoryService.loadSoundFromResources(soundName));
         } catch (UnsupportedAudioFileException e) {
@@ -31,10 +44,18 @@ public class AudioService {
         }
     }
 
+    /**
+     * Plays a sound by reading the provided audio stream
+     * @param stream
+     *  Audio stream to read
+     */
     private static void playSound(AudioInputStream stream){
+        if (audioClip == null) return;
+        if (audioClip.isRunning()) audioClip.stop();
+        if (audioClip.isOpen()) audioClip.close();
         try {
-            audioClip.loop(0);
             audioClip.open(stream);
+            audioClip.start();
         } catch (LineUnavailableException e) {
             System.out.println("Could not open line from Mixer.");
         } catch (IOException e) {
@@ -42,26 +63,42 @@ public class AudioService {
         }
     }
 
+    /**
+     * Plays a sound for success
+     */
     public static void playVictorySound(){
         AudioInputStream ais;
-        ais = getSound("success_2.wav");
+        int soundNumber = new Random().nextInt(2)+1;
+        ais = getSound("success_"+soundNumber+".wav");
         playSound(ais);
     }
 
+    /**
+     * Plays a sound for failure
+     */
     public static void playFailureSound(){
         AudioInputStream ais;
-        ais = getSound("wrong_buzz_1.wav");
+        int soundNumber = new Random().nextInt(2)+1;
+        ais = getSound("wrong_buzz_"+soundNumber+".wav");
         playSound(ais);
     }
 
+    /**
+     * Plays a sound for completely losing
+     */
     public static void playLossSound(){
         AudioInputStream ais;
         ais = getSound("final_loss.wav");
         playSound(ais);
     }
 
+    /**
+     * Plays a sound for losing when the player is out of time
+     */
     public static void playOutOfTimeSound(){
-        //TODO
+        AudioInputStream ais;
+        ais = getSound("final_loss.wav");
+        playSound(ais);
     }
 
 }
